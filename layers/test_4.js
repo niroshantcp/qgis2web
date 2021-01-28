70f2ede36ca1ca7aaa46e39ec47893a8791a1415
niroshantcp <78165986+niroshantcp@users.noreply.github.com>
2021-01-28 22:29:07 +0530

    Initial commit

---
+++ layers/test_4.js
@@ -0,0 +1,8 @@
+var json_test_4 = {
+"type": "FeatureCollection",
+"name": "test_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "test": "<img scr\"http:\/\/mapmint.media.osaka-cu.ac.jp\/lib\/img\/citra.png\" \/>" }, "geometry": { "type": "Point", "coordinates": [ 80.021720985613143, 8.365219047676252 ] } }
+]
+}
